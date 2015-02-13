29126c314727777e8a944017ad25b68d1e1c6d55
AnTi <anti@beta.delfosia.net>
2015-02-13 16:58:05 +0100

    Ajout fichiers nécessaires pour tournoi

---
+++ monequipe.py
@@ -0,0 +1,19 @@
+from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam
+from soccersimulator import PygletObserver,ConsoleListener,LogListener
+from soccersimulator import pyglet
+from strategies import StaticStrategy, RandomStrategy, RunnerStrategy, YoloStrategy
+
+team1=SoccerTeam("team1")
+team1.add_player(SoccerPlayer("t1j1",RunnerStrategy()))
+
+team2=SoccerTeam("team2")
+team2.add_player(SoccerPlayer("t2j1",RunnerStrategy()))
+team2.add_player(SoccerPlayer("t2j2",RunnerStrategy()))
+
+team4=SoccerTeam("team4")
+team4.add_player(SoccerPlayer("t4j1",RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j2",RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j3",RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j4",RunnerStrategy()))
+
+teams =[team1, team2, team4]
