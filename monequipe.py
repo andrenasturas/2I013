e33bfa9ef4487cb9f0434a6b934af29c04487bd5
AnTi <anti@beta.delfosia.net>
2015-02-21 12:18:54 +0100

    Réorganisation équipes

--- monequipe.py
+++ monequipe.py
@@ -3,17 +3,17 @@ from soccersimulator import PygletObserver,ConsoleListener,LogListener
 from soccersimulator import pyglet
 from strategies import StaticStrategy, RandomStrategy, RunnerStrategy, YoloStrategy
 
-team1=SoccerTeam("team1")
-team1.add_player(SoccerPlayer("t1j1",RunnerStrategy()))
+team1 = SoccerTeam("team1")
+team1.add_player(SoccerPlayer("t1j1", RunnerStrategy()))
 
-team2=SoccerTeam("team2")
-team2.add_player(SoccerPlayer("t2j1",RunnerStrategy()))
-team2.add_player(SoccerPlayer("t2j2",RunnerStrategy()))
+team2 = SoccerTeam("team2")
+team2.add_player(SoccerPlayer("t2j1", RunnerStrategy()))
+team2.add_player(SoccerPlayer("t2j2", InterceptorStrategy()))
 
-team4=SoccerTeam("team4")
-team4.add_player(SoccerPlayer("t4j1",RunnerStrategy()))
-team4.add_player(SoccerPlayer("t4j2",RunnerStrategy()))
-team4.add_player(SoccerPlayer("t4j3",RunnerStrategy()))
-team4.add_player(SoccerPlayer("t4j4",RunnerStrategy()))
+team4 = SoccerTeam("team4")
+team4.add_player(SoccerPlayer("t4j1", RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j2", InterceptorStrategy()))
+team4.add_player(SoccerPlayer("t4j3", RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j4", InterceptorStrategy()))
 
-teams =[team1, team2, team4]
+teams = [team1, team2, team4]
