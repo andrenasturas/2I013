8f5dd050aabadbe360d8acc9be9c5f22d127cf41
AnTi <anti@beta.delfosia.net>
2015-02-21 10:26:13 +0100

    Réorganisation des fichiers de test

---
+++ main.py
@@ -0,0 +1,24 @@
+from soccersimulator import pyglet
+from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam
+from soccersimulator import PygletObserver, ConsoleListener, LogListener
+from strategies import *
+
+team1=SoccerTeam("team1")
+team1.add_player(SoccerPlayer("t1j1",RunnerStrategy()))
+
+team2=SoccerTeam("team2")
+team2.add_player(SoccerPlayer("t2j1",RunnerStrategy()))
+team2.add_player(SoccerPlayer("t2j2",RunnerStrategy()))
+
+team4=SoccerTeam("team4")
+team4.add_player(SoccerPlayer("t4j1",RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j2",RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j3",RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j4",RunnerStrategy()))
+
+teams = [team1, team2, team4]
+
+battle=SoccerBattle(teams[2],teams[3])
+obs=PygletObserver()
+obs.set_soccer_battle(battle)
+pyglet.app.run()
