dbe4e414061a64279280fb02724ca9ed32adafbf
AnTi <anti@beta.delfosia.net>
2015-02-23 16:21:41 +0100

    Correction main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam
 from soccersimulator import PygletObserver, ConsoleListener, LogListener
 from strategies import *
 
-team1=SoccerTeam("team1")
+team1 = SoccerTeam("team1")
 team1.add_player(SoccerPlayer("t1j1",RunnerStrategy()))
 
-team2=SoccerTeam("team2")
+team2 = SoccerTeam("team2")
 team2.add_player(SoccerPlayer("t2j1",RunnerStrategy()))
 team2.add_player(SoccerPlayer("t2j2",RunnerStrategy()))
 
-team4=SoccerTeam("team4")
+team4 = SoccerTeam("team4")
 team4.add_player(SoccerPlayer("t4j1",RunnerStrategy()))
 team4.add_player(SoccerPlayer("t4j2",RunnerStrategy()))
 team4.add_player(SoccerPlayer("t4j3",RunnerStrategy()))
@@ -18,7 +18,7 @@ team4.add_player(SoccerPlayer("t4j4",RunnerStrategy()))
 
 teams = [team1, team2, team4]
 
-battle=SoccerBattle(teams[2],teams[3])
+battle =SoccerBattle(teams[2],teams[2])
 obs=PygletObserver()
 obs.set_soccer_battle(battle)
 pyglet.app.run()
