312933331888dc8aa2ab8b1a9f5157642f52e9de
AnTi <anti@beta.delfosia.net>
2015-03-01 09:26:54 +0100

    Modification composition équipes

--- monequipe.py
+++ monequipe.py
@@ -4,16 +4,16 @@ from soccersimulator import pyglet
 from strategies import RunnerStrategy, InterceptorStrategy
 
 team1 = SoccerTeam("team1")
-team1.add_player(SoccerPlayer("t1j1", RunnerStrategy()))
+team1.add_player(SoccerPlayer("t1j1", InterceptorStrategy()))
 
 team2 = SoccerTeam("team2")
-team2.add_player(SoccerPlayer("t2j1", RunnerStrategy()))
+team2.add_player(SoccerPlayer("t2j1", InterceptorStrategy()))
 team2.add_player(SoccerPlayer("t2j2", InterceptorStrategy()))
 
 team4 = SoccerTeam("team4")
-team4.add_player(SoccerPlayer("t4j1", RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j1", InterceptorStrategy()))
 team4.add_player(SoccerPlayer("t4j2", InterceptorStrategy()))
-team4.add_player(SoccerPlayer("t4j3", RunnerStrategy()))
+team4.add_player(SoccerPlayer("t4j3", InterceptorStrategy()))
 team4.add_player(SoccerPlayer("t4j4", InterceptorStrategy()))
 
 teams = [team1, team2, team4]
