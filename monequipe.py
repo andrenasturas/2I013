bd0b0e03932524b3a70b7ee3a8e34bb0ef28172a
AnTi <anti@beta.delfosia.net>
2015-03-23 11:03:35 +0100

    Modification monequipe (Merge manuel)

--- monequipe.py
+++ monequipe.py
@@ -1,19 +1,19 @@
 from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam
 from soccersimulator import PygletObserver,ConsoleListener,LogListener
 from soccersimulator import pyglet
-from strategies import RunnerStrategy, InterceptorStrategy
+from strategies import runner
 
-team1 = SoccerTeam("team1")
-team1.add_player(SoccerPlayer("t1j1", InterceptorStrategy()))
+team1 = SoccerTeam("Aventurier Errant")
+team1.add_player(SoccerPlayer("t1j1", runner))
 
-team2 = SoccerTeam("team2")
-team2.add_player(SoccerPlayer("t2j1", InterceptorStrategy()))
-team2.add_player(SoccerPlayer("t2j2", InterceptorStrategy()))
+team2 = SoccerTeam("Aventuriers Errants")
+team2.add_player(SoccerPlayer("t2j1", runner))
+team2.add_player(SoccerPlayer("t2j2", runner))
 
-team4 = SoccerTeam("team4")
-team4.add_player(SoccerPlayer("t4j1", InterceptorStrategy()))
-team4.add_player(SoccerPlayer("t4j2", InterceptorStrategy()))
-team4.add_player(SoccerPlayer("t4j3", InterceptorStrategy()))
-team4.add_player(SoccerPlayer("t4j4", InterceptorStrategy()))
+team4 = SoccerTeam("Aventuriers Errants")
+team4.add_player(SoccerPlayer("t4j1", runner))
+team4.add_player(SoccerPlayer("t4j2", runner))
+team4.add_player(SoccerPlayer("t4j3", runner))
+team4.add_player(SoccerPlayer("t4j4", runner))
 
 teams = [team1, team2, team4]
