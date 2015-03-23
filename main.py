ecdd65b8bdac9c6c25203528d2266912ebb43c62
AnTi <anti@beta.delfosia.net>
2015-03-23 11:03:06 +0100

    Modification main (Merge manuel)

--- main.py
+++ main.py
@@ -3,28 +3,20 @@ from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam
 from soccersimulator import PygletObserver, ConsoleListener, LogListener
 from strategies import *
 
-team1 = SoccerTeam("team1")
-team1.add_player(SoccerPlayer("t1j1",RunnerStrategy()))
+team1=SoccerTeam("Aventurier Errant")
+team1.add_player(SoccerPlayer("t1j1", runner))
 
-team2 = SoccerTeam("team2")
-team2.add_player(SoccerPlayer("t2j1",RunnerStrategy()))
-team2.add_player(SoccerPlayer("t2j2",RunnerStrategy()))
+team2=SoccerTeam("Aventuriers Errants Coureurs")
+team2.add_player(SoccerPlayer("t2j1", runner))
+team2.add_player(SoccerPlayer("t2j2", runner))
 
-team4 = SoccerTeam("team4")
-team4.add_player(SoccerPlayer("t4j1",RunnerStrategy()))
-team4.add_player(SoccerPlayer("t4j2",RunnerStrategy()))
-team4.add_player(SoccerPlayer("t4j3",RunnerStrategy()))
-team4.add_player(SoccerPlayer("t4j4",RunnerStrategy()))
+team4=SoccerTeam("Aventuriers Errants Intercepteurs")
+team4.add_player(SoccerPlayer("t4j1", interc))
+team4.add_player(SoccerPlayer("t4j2", interc))
 
-team8 = SoccerTeam("team8")
-team8.add_player(SoccerPlayer("t8j1",InterceptorStrategy()))
-team8.add_player(SoccerPlayer("t8j2",InterceptorStrategy()))
-team8.add_player(SoccerPlayer("t8j3",InterceptorStrategy()))
-team8.add_player(SoccerPlayer("t8j4",InterceptorStrategy()))
+teams = [team1, team2, team4]
 
-teams = [team1, team2, team4, team8]
-
-battle =SoccerBattle(teams[2],teams[3])
-obs=PygletObserver()
+battle = SoccerBattle(teams[2], teams[1])
+obs = PygletObserver()
 obs.set_soccer_battle(battle)
 pyglet.app.run()
