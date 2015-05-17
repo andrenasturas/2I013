f7032036ee7562661780d094380d1c9802feb916
AnTi <anti@delfosia.net>
2015-05-17 20:59:57 +0200

    Mise à jour équipes

--- monequipe.py
+++ monequipe.py
@@ -1,21 +1,21 @@
 from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam
 from soccersimulator import PygletObserver,ConsoleListener,LogListener
 from soccersimulator import pyglet
-from strategies import runner
+from strategies import RunnerStrategy, DefendStrategy
 from apprentissage import *
 
 team1 = SoccerTeam("Aventurier Errant")
-team1.add_player(SoccerPlayer("t1j1", runner))
+team1.add_player(SoccerPlayer("t1j1", RunnerStrategy("Runner")))
 
 team2 = SoccerTeam("Aventuriers Errants")
-team2.add_player(SoccerPlayer("t2j1", runner))
-team2.add_player(SoccerPlayer("t2j2", runner))
+team2.add_player(SoccerPlayer("t2j1", RunnerStrategy("Runner")))
+team2.add_player(SoccerPlayer("t2j2", DefendStrategy("Defend")))
 
 team4 = SoccerTeam("Aventuriers Errants")
-team4.add_player(SoccerPlayer("t4j1", runner))
-team4.add_player(SoccerPlayer("t4j2", runner))
-team4.add_player(SoccerPlayer("t4j3", runner))
-team4.add_player(SoccerPlayer("t4j4", runner))
+team4.add_player(SoccerPlayer("t4j1", RunnerStrategy("Runner")))
+team4.add_player(SoccerPlayer("t4j2", RunnerStrategy("Runner")))
+team4.add_player(SoccerPlayer("t4j3", DefendStrategy("Defend")))
+team4.add_player(SoccerPlayer("t4j4", DefendStrategy("Defend")))
 
 team_tree = SoccerTeam("Team Tree")
 treeia=TreeIA(gen_feature_simple,dict({"Random":RandomStrategy(),"Fonceur":FonceurStrategy()}))
